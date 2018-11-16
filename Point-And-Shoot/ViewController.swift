fa026f7b2879d9fbe2855170fecd623bb4117a57
krabbkakan <erikhede@gmail.com>
2018-11-16 12:46:04 +0100

    Initial Commit

---
+++ Point-And-Shoot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Point-And-Shoot
+//
+//  Created by Erik Hede on 16/11/2018.
+//  Copyright © 2018 Erik Hede. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
